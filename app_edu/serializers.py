3da6fcad3ff1c70e872182b79f7aea26b22d3f03
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-02-14 20:03:09 +0700

    change course serializer - add sub model serializer

--- app_edu/serializers.py
+++ app_edu/serializers.py
@@ -3,18 +3,19 @@ from rest_framework import serializers
 from app_edu.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        exclude = ('courses',)
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True)
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
 
     class Meta:
         model = Course
-        fields = '__all__'
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
         fields = '__all__'
