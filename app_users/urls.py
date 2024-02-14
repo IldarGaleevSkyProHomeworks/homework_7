b2f9fa1ceab13e7b6804ed785152d5cfe174fbf5
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-02-14 20:25:30 +0700

    add payments serializers + view + user payment field.

--- app_users/urls.py
+++ app_users/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
 from app_users.apps import AppUsersConfig
-from app_users.views import UserViewSet
+from app_users.views import UserViewSet, PaymentsViewSet
 
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='users')
+router.register('payments', PaymentsViewSet, basename='payments')
 
 app_name = AppUsersConfig.name
 
--- app_users/views.py
+++ app_users/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from app_users.models import User
-from app_users.serializers import UserSerializer
+from app_users.models import User, Payment
+from app_users.serializers import UserSerializer, PaymentSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class PaymentsViewSet(viewsets.ModelViewSet):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
