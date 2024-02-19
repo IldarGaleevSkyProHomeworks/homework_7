e4b71b8ef8f6b6ed7c0ba8b708085febac3e28f7
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-02-19 17:56:25 +0700

    add the owner field for the lesson and course models

--- app_edu/serializers.py
+++ app_edu/serializers.py
@@ -4,6 +4,14 @@ from app_edu.models import Course, Lesson
 
 
 class LessonSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        curr_user = self.context['request'].user
+        new_lesson = super().create(validated_data)
+        new_lesson.owner = curr_user
+        new_lesson.save()
+        return new_lesson
+
     class Meta:
         model = Lesson
         exclude = ('courses',)
@@ -11,10 +19,17 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
-    lessons = LessonSerializer(many=True)
+    lessons = LessonSerializer(many=True, required=False)
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
+
+    def create(self, validated_data):
+        curr_user = self.context['request'].user
+        new_course = super().create(validated_data)
+        new_course.owner = curr_user
+        new_course.save()
+        return new_course
 
     class Meta:
         model = Course
