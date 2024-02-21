cb6c6fc95d608ab56f2612e2e4d81203d07e1643
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-02-21 20:44:18 +0700

    add new permission classes
    - IsOwner
    - IsManager
    - IsContentCreator

--- app_users/apps.py
+++ app_users/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AppUsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app_users"
+
+    manager_group_name = 'manager'
+    content_creator_group_name = 'creator'
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -60,6 +61,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ]
 }
+
+if DEBUG:
+    SIMPLE_JWT = {
+        'ACCESS_TOKEN_LIFETIME': timedelta(days=30)
+    }
 
 ROOT_URLCONF = "config.urls"
 
