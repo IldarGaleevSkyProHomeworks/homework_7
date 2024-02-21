fa4f56e558f6e1f1a957fd8139d32beff7ffb268
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-02-21 21:47:15 +0700

    fix: delete safe methods from custom permissions

--- app_users/permissions.py
+++ app_users/permissions.py
@@ -17,22 +17,16 @@ class IsAnonCreate(permissions.BasePermission):
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
+        # if request.method in permissions.SAFE_METHODS:
+        #     return True
         return obj.owner == request.user if hasattr(obj, 'owner') else False
 
 
 class IsManager(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
         return request.user.groups.filter(name=AppUsersConfig.manager_group_name).exists()
 
 
 class IsContentCreator(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
         return request.user.groups.filter(name=AppUsersConfig.content_creator_group_name).exists()
