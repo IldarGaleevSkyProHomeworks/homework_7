b70d9b617cded891d4d1809087ad3fbdb7457a78
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-02-21 21:48:02 +0700

    add: check permissions for lessons and courses

--- app_edu/views/lesson_view.py
+++ app_edu/views/lesson_view.py
@@ -1,29 +1,36 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from app_edu.models import Lesson
 from app_edu.serializers import LessonSerializer
+from app_users.permissions import IsOwner, IsManager, IsContentCreator
 
 
 class LessonListViewSet(generics.ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class LessonRetrieveView(generics.RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsOwner | IsManager,)
 
 
 class LessonCreateView(generics.CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsContentCreator,)
 
 
 class LessonDeleteView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsOwner,)
 
 
 class LessonUpdateView(generics.UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsOwner | IsManager,)
