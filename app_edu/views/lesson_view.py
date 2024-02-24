d2f7587fed0bb31826f8c1cd6eae5b53db3c3878
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-02-24 09:16:35 +0700

    fix: wrong permissions

--- app_edu/views/lesson_view.py
+++ app_edu/views/lesson_view.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from app_edu.models import Lesson
 from app_edu.serializers import LessonSerializer
+from app_users.apps import AppUsersConfig
 from app_users.permissions import IsOwner, IsManager, IsContentCreator
 
 
@@ -10,6 +11,11 @@ class LessonListViewSet(generics.ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        if self.request.user.groups.filter(name=AppUsersConfig.manager_group_name).exists():
+            return Lesson.objects.all()
+        return Lesson.objects.filter(owner=self.request.user)
 
 
 class LessonRetrieveView(generics.RetrieveAPIView):
