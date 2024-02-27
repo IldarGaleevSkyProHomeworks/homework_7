66aa60f863684dec94b61d13f726431ab6a3f5a7
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-02-27 22:42:50 +0700

    add the subscriptions field to the user serializer

--- app_edu/serializers/subscription_serializer.py
+++ app_edu/serializers/subscription_serializer.py
@@ -4,6 +4,14 @@ from app_edu.models import Subscription
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        remove_fields = kwargs.pop('remove_fields', None)
+        super().__init__(*args, **kwargs)
+        if remove_fields:
+            for field_name in remove_fields:
+                self.fields.pop(field_name)
+
     class Meta:
         model = Subscription
         fields = '__all__'
